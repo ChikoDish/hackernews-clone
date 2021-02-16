169f393c0e58cf02d677e693242f0cc48ab7d496
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-02-16 09:50:25 +0530

    added Loader

--- src/components/ShowStories.js
+++ src/components/ShowStories.js
@@ -2,20 +2,18 @@ import React from "react";
 import { Fragment } from "react";
 import useFetchData from "../hooks/FatchData";
 import Blog from "./Blog";
+import Loader from "./Loader";
 
 const ShowStories = ({ type }) => {
   const { isLoading, stories } = useFetchData(type ? type : "top");
   return (
     <Fragment>
-      {isLoading ? (
-        <p className="loading">Loading...</p>
-      ) : (
-        <React.Fragment>
-          {stories.map(({ data: story }) => (
-            <Blog key={story.id} story={story} />
-          ))}
-        </React.Fragment>
-      )}
+      <Loader show={isLoading}>Loading...</Loader>
+      <React.Fragment>
+        {stories.map(({ data: story }) => (
+          <Blog key={story.id} story={story} />
+        ))}
+      </React.Fragment>
     </Fragment>
   );
 };
